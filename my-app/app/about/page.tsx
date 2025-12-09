951fb4b91c07ea586d45121042cbf44a6435783a
MarcelLeonMrgic <marcelmrgic@gmail.com>
2025-12-09 22:03:31 +0100

    added about

---
+++ my-app/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function About() {
+  return (
+   <div>
+    Hi ich heiße Marcel Léon Mrgic. Das ist ein kleines Projekt auf das ich Bock hatte. Wenn du dich für mich interessierst kannst du mir gerne mal eine Mail schreiben: marcelmrgic@gmail.com 
+
+    </div>
+  );
+}
--- my-app/components/Menu.tsx
+++ my-app/components/Menu.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Link from 'next/link'
 const Menu = () => {
   return (
-    <nav>
+    <nav className='py-4 border-b-4 border-black'>
         <ul className='flex justify-between'>
             <li><Link href="/">HealthFake</Link></li>
             <div className='flex'>
-            <li><Link href="/about">About Mrgic</Link></li>
-            <li><Link href="/login">Register</Link></li>
+            <li><Link href="/about" className=''>About Mrgic</Link></li>
+            <li><Link href="/login" className='py-4 px-8 border-4'>Register</Link></li>
             </div>
             
         </ul>
